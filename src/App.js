f652cd7b97f433279b32257724720cead631a8df
laiola <laiola1003@gmail.com>
2019-02-03 19:20:19 +0300

    reworked Characteristics to stateless component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React, { Component } from 'react';
-import Characteristics from './components/characteristics/Characteristics';
+import React, { Component } from "react";
+import { Characteristics } from "./components/characteristics/Characteristics";
+import { calculateTotalIntake } from "././helper/nutritionCalculator";
+import { FEMALE } from "./constant/Sex";
+import { NORMAL_ACTIVITY } from "./constant/Activity";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      weight: 0,
+      height: 0,
+      age: 0,
+      sex: FEMALE,
+      activity: NORMAL_ACTIVITY
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    alert(calculateTotalIntake(this.state));
+  };
+
   render() {
     return (
-      <Characteristics/>
+      <Characteristics
+        sex={this.state.sex}
+        activity={this.state.activity}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
     );
   }
 }
--- src/components/characteristics/Characteristics.js
+++ src/components/characteristics/Characteristics.js
@@ -1,74 +1,46 @@
-import React, { Component } from 'react';
-import { calculateTotalIntake } from '../../helper/nutritionCalculator';
+import React from 'react';
 import { MALE, FEMALE } from '../../constant/Sex';
 import { NORMAL_ACTIVITY, NO_ACTIVITY, LOW_ACTIVITY, HIGH_ACTIVITY, EXTREME_ACTIVITY } from '../../constant/Activity';
 
-class Characteristics extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            weight: 0,
-            height: 0,
-            age: 0,
-            sex: FEMALE,
-            activity: NORMAL_ACTIVITY,
-        }
-    }
-
-    handleChange = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    };
-
-    handleSubmit = () => {
-        alert(calculateTotalIntake(this.state));
-    };
-
-    render() {
-        return (
-            <div className="characteristics">
-                <form onSubmit={this.handleSubmit}>
+export const Characteristics = props => {
+    const { handleChange } = props;
+    
+    return (
+        <div className="characteristics">
+            <form onSubmit={props.handleSubmit}>
+                <label>
+                    Pick your sex:
+                    <select value={props.sex} onChange={handleChange} name="sex">
+                        <option value={MALE}>Male</option>
+                        <option value={FEMALE}>Female</option>
+                    </select>
+                </label>
+                <div>
                     <label>
-                        Pick your sex:
-                        <select value={this.state.sex} onChange={this.handleChange} name="sex">
-                            <option value={MALE}>Male</option>
-                            <option value={FEMALE}>Female</option>
-                        </select>
+                        Weight in kg:
+                        <input type="number" step="1" name="weight" onChange={handleChange} />
                     </label>
-                    <div>
-                        <label>
-                            Weight in kg:
-                            <input type="number" step="1" name="weight" 
-                                value={this.state.weight} onChange={this.handleChange} />
-                        </label>
-                        <label>
-                            Height in cm:
-                            <input type="number" step="1" name="height" 
-                                value={this.state.height} onChange={this.handleChange} />
-                        </label>
-                        <label>
-                            Age in years:
-                            <input type="number" step="1" name="age" 
-                                value={this.state.age} onChange={this.handleChange} />
-                        </label>
-                    </div>
                     <label>
-                        Pick your lifestyle:
-                        <select value={this.state.activity} onChange={this.handleChange} name="activity">
-                            <option value={NO_ACTIVITY}>No activity</option>
-                            <option value={LOW_ACTIVITY}>Low activity</option>
-                            <option value={NORMAL_ACTIVITY}>Normal activity</option>
-                            <option value={HIGH_ACTIVITY}>High activity</option>
-                            <option value={EXTREME_ACTIVITY}>Extreme activity</option>
-                        </select>
+                        Height in cm:
+                        <input type="number" step="1" name="height" onChange={handleChange} />
                     </label>
-                    <input type="submit" value="Submit"/>
-                </form>
-            </div>
-        );
-    }
-}
-
-export default Characteristics;
+                    <label>
+                        Age in years:
+                        <input type="number" step="1" name="age" onChange={handleChange} />
+                    </label>
+                </div>
+                <label>
+                    Pick your lifestyle:
+                    <select value={props.activity} onChange={handleChange} name="activity">
+                        <option value={NO_ACTIVITY}>No activity</option>
+                        <option value={LOW_ACTIVITY}>Low activity</option>
+                        <option value={NORMAL_ACTIVITY}>Normal activity</option>
+                        <option value={HIGH_ACTIVITY}>High activity</option>
+                        <option value={EXTREME_ACTIVITY}>Extreme activity</option>
+                    </select>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
+};
