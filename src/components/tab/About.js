8e6b66839aeffb31c70841882970f44c127e03d2
laiola <laiola1003@gmail.com>
2019-04-02 17:11:08 +0300

    added project info

--- src/components/tab/About.js
+++ src/components/tab/About.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
 
 import { Header } from '../header/Header';
+import MathJax from 'react-mathjax2'
+
+import './About.css';
+
+const BMR_EQUATION = 'P = \\left ( \\frac {10.0 m} {1 ~ \\mbox {kg}} + \\frac {6.25 h} {1 ~ \\mbox {cm}} - \\frac {5.0 a} {1 ~ \\mbox {year}} + s \\right ) \\frac {\\mbox {kcal}} {\\mbox {day}}';
+const TOTAL_INTAKE = 'a * BMR';
+const NUTRITION = `\\begin{multline}
+\\shoveleft fat = weight * fatRatio\\\\
+\\shoveleft protein = weight * ProteinRatio\\\\
+\\shoveleft carbohydrate = \\frac{goalIntake - fat * 9 - protein * 4}{4}
+\\end{multline}`;
 
 class About extends Component {
     render() {
         return (
             <div>
                 <Header/>
-                <p className="lead">
-                    Info about application.
-                </p>
-                <p>
-                    How does calculation work?
-                    Info about features.
-                </p>
+                <div className="about">
+                    <h5 className="text-primary">Step 1. Calculate your basal metabolic rate(BMR)</h5>
+                    <p className="lead">
+                        The Mifflin St Jeor Equation:
+                    </p>
+                    <div className="paragraph">
+                        <MathJax.Context input='tex'>
+                            <div>
+                                <MathJax.Node inline>{BMR_EQUATION}</MathJax.Node>
+                                , where s is +5 for males and −161 for females.
+                            </div>
+                        </MathJax.Context>
+                    </div>
+
+                    <h5 className="text-primary">Step 2. Calculate your total intake</h5>
+                    <div className="paragraph">
+                        <MathJax.Context input='tex'>
+                            <div>
+                                <MathJax.Node inline>{TOTAL_INTAKE}</MathJax.Node>
+                                , where a is your activity coefficient.
+                            </div>
+                        </MathJax.Context>
+                    </div>
+
+                    <h5 className="text-primary">Step 3. Calculate your nutrition</h5>
+                    <div className="paragraph">
+                        <MathJax.Context input='tex'>
+                            <div>
+                                <MathJax.Node>{NUTRITION}</MathJax.Node>
+                                We recommend use at least 1 gr fat per 1 kg weight,
+                                and doesn't user more then 1.5 gr protein per 1 kg weight.
+                            </div>
+                        </MathJax.Context>
+                    </div>
+                </div>
             </div>
         )
     }
