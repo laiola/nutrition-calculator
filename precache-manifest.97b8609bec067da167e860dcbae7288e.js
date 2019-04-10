22fd40915ec3ecd75c4fdfb1244a2de4a8f0940d
laiola <laiola1003@gmail.com>
2019-04-10 15:39:55 +0300

    Updates

---
+++ precache-manifest.97b8609bec067da167e860dcbae7288e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c6ec8ec0eab70bacf9f0c9735aa507c",
+    "url": "/nutrition-calculator/static/media/logo.6c6ec8ec.jpg"
+  },
+  {
+    "revision": "3e68ec08e876b6a12bab",
+    "url": "/nutrition-calculator/static/js/runtime~main.3e68ec08.js"
+  },
+  {
+    "revision": "d127dc2952b08d611c23",
+    "url": "/nutrition-calculator/static/js/main.d127dc29.chunk.js"
+  },
+  {
+    "revision": "3488ccb4f15216030c4c",
+    "url": "/nutrition-calculator/static/js/1.3488ccb4.chunk.js"
+  },
+  {
+    "revision": "d127dc2952b08d611c23",
+    "url": "/nutrition-calculator/static/css/main.f2e2bd84.chunk.css"
+  },
+  {
+    "revision": "3488ccb4f15216030c4c",
+    "url": "/nutrition-calculator/static/css/1.4c767a5d.chunk.css"
+  },
+  {
+    "revision": "2dea2da14f506976c1e4242a48c5398c",
+    "url": "/nutrition-calculator/index.html"
+  }
+];
